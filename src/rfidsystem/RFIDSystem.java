93f315e6c63da008d1498b77ccb7d72dba44bb08
James <jman1118@yahoo.com>
2017-03-13 19:42:04 -0400

    Fixed some git commit merge error

--- src/rfidsystem/RFIDSystem.java
+++ src/rfidsystem/RFIDSystem.java
@@ -4,15 +4,9 @@
  * Copyright James Vanaselja 2017. All Rights Reserved.
  */
 package rfidsystem;
-<<<<<<< HEAD
-import java.awt.*;
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-=======
 
-import java.util.ArrayList;
->>>>>>> 237384f71c86692ba120c2c56ae1777d033a0cbd
+import java.awt.*;
+import java.util.*;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 
@@ -21,28 +15,21 @@ import javax.swing.JTextField;
  * @author James
  */
 public class RFIDSystem {
-<<<<<<< HEAD
+
     //hardwired tag id
     private static final String Allowed = "0002104953";
-=======
-   //String of the test ID
-   // private static final String Allowed = "0002104953";
->>>>>>> 237384f71c86692ba120c2c56ae1777d033a0cbd
 
+    //String of the test ID
+    // private static final String Allowed = "0002104953";
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         Boolean bool = true;
-        
+
         Object[] options = new String[]{"Add A Tag", "Remove A Tag", "List All IDs", "Check Tag", "Exit"};
         JTextField field = new JTextField(20);
-        
-<<<<<<< HEAD
-        
-        
-=======
->>>>>>> 237384f71c86692ba120c2c56ae1777d033a0cbd
+
         ArrayList list = new ArrayList();
         Object[] message = {
             "Scan Tag Now:\n", field
@@ -53,18 +40,12 @@ public class RFIDSystem {
                     null, options, null);
 
             if (option == 0) {
-               // System.out.println("Option: " + option);
-<<<<<<< HEAD
-                
-                addToList(field.getText(), list);
-               
-=======
+                // System.out.println("Option: " + option);
 
                 addToList(field.getText(), list);
 
->>>>>>> 237384f71c86692ba120c2c56ae1777d033a0cbd
             } else if (option == 1) {
-               // System.out.println("Option: " + option);
+                // System.out.println("Option: " + option);
                 removeFromList(field.getText(), list);
 
             } else if (option == 2) {
@@ -76,8 +57,8 @@ public class RFIDSystem {
             } else {
                 bool = false;
                 /*System.out.println("option: " + option);
-                System.out.println("Tag Reading aborted");
-                */
+                 System.out.println("Tag Reading aborted");
+                 */
                 System.exit(0);
             }
         }
@@ -85,48 +66,35 @@ public class RFIDSystem {
 
     public static void addToList(String field, ArrayList list) {
         String compare = "";
-<<<<<<< HEAD
         String message = "";
         field = sanitize(field);
-        compare = field;    
- 
-        
-        if (field.isEmpty()){
+        compare = field;
+
+        if (field.isEmpty()) {
             Toolkit.getDefaultToolkit().beep();
             message += "Please scan or enter a tag number";
             JOptionPane.showMessageDialog(null, message, "Notice", 1);
-             
-        }
-        else if (list.isEmpty() || !list.contains(compare)) {
-            list.add(field);
-            
-=======
-        compare = field;
 
-        if (list.isEmpty() || !list.contains(compare)) {
+        } else if (list.isEmpty() || !list.contains(compare)) {
             list.add(field);
 
->>>>>>> 237384f71c86692ba120c2c56ae1777d033a0cbd
-            System.out.println(list);
-        } else {
-            for (int i = 0; i < list.size(); i++) {
-                System.out.println(list.get(i));
+            if (list.isEmpty() || !list.contains(compare)) {
+                list.add(field);
+
+                System.out.println(list);
+            } else {
+                for (int i = 0; i < list.size(); i++) {
+                    System.out.println(list.get(i));
+                }
             }
         }
-<<<<<<< HEAD
-        
-=======
->>>>>>> 237384f71c86692ba120c2c56ae1777d033a0cbd
     }
 
     public static void removeFromList(String field, ArrayList list) {
         //Removes the entered ID from the program.
         String compareTo = "";
         String message = "";
-<<<<<<< HEAD
-        
-=======
->>>>>>> 237384f71c86692ba120c2c56ae1777d033a0cbd
+
         compareTo = field;
         if (list.contains(compareTo)) {
             list.remove(compareTo);
@@ -134,17 +102,17 @@ public class RFIDSystem {
             JOptionPane.showMessageDialog(null, message, "Notice", 1);
         } else if (list.isEmpty()) {
             //System.out.println("List is empty");
-<<<<<<< HEAD
-             Toolkit.getDefaultToolkit().beep();
+
+            Toolkit.getDefaultToolkit().beep();
             JOptionPane.showMessageDialog(null, "Unable to remove item. "
                     + "List is currently empty", "Alert", 1);
         } else if (!list.contains(compareTo)) {
-             Toolkit.getDefaultToolkit().beep();
-=======
+            Toolkit.getDefaultToolkit().beep();
+
             JOptionPane.showMessageDialog(null, "Unable to remove item. "
                     + "List is currently empty", "Alert", 1);
         } else if (!list.contains(compareTo)) {
->>>>>>> 237384f71c86692ba120c2c56ae1777d033a0cbd
+
             JOptionPane.showMessageDialog(null, "Unable to remove item. "
                     + "List does not contain ID#" + compareTo, "Alert", 1);
             //System.out.println("List does not contain the requested item");
@@ -154,11 +122,10 @@ public class RFIDSystem {
 
     public static void validateID(String field, ArrayList list) {
         /*Compares the number entered into the textfield to the list of IDs
-        in the system.
-        */
+         in the system.
+         */
         String msg = "";
-<<<<<<< HEAD
-        
+
         if (list.contains(field)) {
             //System.out.println("Access Allowed");
             msg += "Access Granted!";
@@ -166,57 +133,38 @@ public class RFIDSystem {
         } else if (list.isEmpty()) {
             msg += "Access Denied. No Tag ID entered";
             //System.out.println("List is empty!");
-             Toolkit.getDefaultToolkit().beep();
-              JOptionPane.showMessageDialog(null, msg, "Error", 1);
+            Toolkit.getDefaultToolkit().beep();
+            JOptionPane.showMessageDialog(null, msg, "Error", 1);
         } else {
             msg += "Access Denied!";
-             Toolkit.getDefaultToolkit().beep();
+            Toolkit.getDefaultToolkit().beep();
             //System.out.println("Access Denied");
-              JOptionPane.showMessageDialog(null, msg, "Error", 1);
+            JOptionPane.showMessageDialog(null, msg, "Error", 1);
         }
-       
 
     }
-    
-    private static String sanitize(String field){
+
+    private static String sanitize(String field) {
         String clean = "";
-        clean = field.replaceAll("[^\\d]","");
+        clean = field.replaceAll("[^\\d]", "");
         System.out.println("field: " + field + "\nclean: " + clean);
         return clean;
     }
-=======
 
-        if (list.contains(field)) {
-            //System.out.println("Access Allowed");
-            msg += "Access Granted!";
-
-        } else if (list.isEmpty()) {
-            msg += "Access Denied. No Tag ID entered";
-            //System.out.println("List is empty!");
-        } else {
-            msg += "Access Denied!";
-            //System.out.println("Access Denied");
-        }
-        JOptionPane.showMessageDialog(null, msg, "Error", 1);
-
-    }
-
->>>>>>> 237384f71c86692ba120c2c56ae1777d033a0cbd
     public static void printList(ArrayList list) {
         //Prints list of IDs in system.
         int counter = 0;
         String message = "";
-        if(list.isEmpty()){
-                //System.out.println("Error");
+        if (list.isEmpty()) {
+            //System.out.println("Error");
             message += "List is empty";
-<<<<<<< HEAD
-             Toolkit.getDefaultToolkit().beep();
-=======
->>>>>>> 237384f71c86692ba120c2c56ae1777d033a0cbd
-            }
-        
+
+            Toolkit.getDefaultToolkit().beep();
+
+        }
+
         for (Object list1 : list) {
-            
+
             counter++;
             message += counter + ": ID# " + list1 + "\n";
             //System.out.println(message);
